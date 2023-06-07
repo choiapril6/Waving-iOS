892e27c73471ce1cfd646ab6d609e7dc0597e9a9
Jane Choi <choiapril6@gmail.com>
2023-06-07 22:09:02 +0900

    feat: 가입화면 > 스크롤뷰

--- Waving-iOS/Presentation/Signup/ViewController/SignupViewController.swift
+++ Waving-iOS/Presentation/Signup/ViewController/SignupViewController.swift
@@ -17,6 +17,36 @@ final class SignupViewController: UIViewController {
         return tableView
     }()
     
+    lazy private var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -300)
+        ])
+        return scrollView
+    }()
+    
+    lazy private var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,17 +56,39 @@ final class SignupViewController: UIViewController {
     }
     
     private func setupView() {
-        view.addSubview(tableView)
         
-        tableView.snp.makeConstraints { make in
-            make.top.equalToSuperview()
-            make.leading.equalToSuperview()
-            make.trailing.equalToSuperview()
-            make.bottom.equalToSuperview()
-        }
+        let label1 = UILabel()
+        label1.text = "Label 1"
+        stackView.addArrangedSubview(label1)
+
+        let label2 = UILabel()
+        label2.text = "Label 2"
+        stackView.addArrangedSubview(label2)
         
-        tableView.register(SignupViewCell.self
-                           , forCellReuseIdentifier: "SignupViewCell")
+        let label3 = UILabel()
+        label3.text = "Label 3"
+        stackView.addArrangedSubview(label3)
+        
+        let label4 = UILabel()
+        label4.text = "Label 4"
+        stackView.addArrangedSubview(label4)
+        
+        let label5 = UILabel()
+        label5.text = "Label 5"
+        stackView.addArrangedSubview(label5)
+        
+        let label6 = UILabel()
+        label6.text = "Label 6"
+        stackView.addArrangedSubview(label6)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        stackView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+
+        scrollView.contentSize = stackView.bounds.size
+
     }
 }
 
